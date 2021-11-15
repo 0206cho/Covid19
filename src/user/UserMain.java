b57066063eff7edc2875ab85801a6249072ac324
김종하 <81893082+gbr7780@users.noreply.github.com>
2021-11-15 14:48:36 +0900

    유저_메인 우측 상단 코로나19 전광판 + 지역별 2차백신접종율 출력

--- src/user/UserMain.java
+++ src/user/UserMain.java
@@ -3,16 +3,31 @@ package user;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
+import javax.lang.model.util.Elements;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import jdbc.DB;
 import user.comfirmed_case.Distancing_Map;
@@ -22,10 +37,11 @@ import user.vaccine.VaccineIntro;
 
 public class UserMain extends JFrame implements ActionListener  {
 	
-	
+
+
 	
 	private JLabel lblicon;
-	private JPanel p1;
+	private JPanel p1,top_right;
 	private JPanel p2;
 	private Graphics Graphics;
 	private java.awt.Graphics g;
@@ -34,13 +50,37 @@ public class UserMain extends JFrame implements ActionListener  {
 	private JButton b3;
 	private JButton b4;
 	private JButton b5;
-
+	
+	String url = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EC%BD%94%EB%A1%9C%EB%82%98&oquery=%EC%BD%94%EB%A1%9C%EB%82%98&tqi=hh3z4lprvTVssa%2FOlrRssssssv8-379439";
+	org.jsoup.select.Elements confirmed_elm,death_elm,totalCon_elm,totalDea_elm;
+	String today_confirmed,today_death,today_totalCon,today_totalDea;
+	Document doc;
+	private JLabel covid_label;
+	private JPanel last;
+	private JPanel test_panel;
+	private JPanel p6;
+	private JLabel local_label;
+	
+	
 	//JFrame을 상속 받아 만드는 방법 << 이걸 더 선호함.
 	public UserMain(String title, int width, int height) {
 		this.setTitle(title);
 		setSize(width, height);
 		setLocationRelativeTo(this); 	//화면 가운데 찍음
 		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //닫을수 있는 특정 상수값을 주었기 때문에 프레임 종료버튼이 클릭될때 프로그램도 같이 사라짐 
+		
+		// 실행 간격 지정(3600초)
+		int sec = 3600;
+		// 주기적인 작업을 위한
+		final ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
+		exec.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				covidScoreboard();
+				// 에러 발생시 중지시킴
+				exec.shutdown();
+			}
+			}, 0, sec, TimeUnit.SECONDS);
 		
 		p1 = new JPanel();
 		p1.setLayout(new BorderLayout());
@@ -51,7 +91,26 @@ public class UserMain extends JFrame implements ActionListener  {
 		p1.setBackground(Color.white);
 		p1.add(lblicon,BorderLayout.WEST);
 		
+		top_right = new JPanel();
+		top_right.setBackground(Color.white);
+		top_right.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0));
+		top_right.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		covid_label = new JLabel();
+		covid_label.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+//		local_label = new JLabel();
+//		local_label.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+		top_right.add(covid_label);
+//		top_right.add(local_label);
 		
+		p6 = new JPanel();
+		p6.setBackground(Color.white);
+		p6.setLayout(new BorderLayout());
+		p6.add(top_right,BorderLayout.EAST);
+		
+		test_panel = new JPanel();
+		test_panel.setLayout(new BorderLayout());
+		test_panel.add(p1,BorderLayout.WEST);
+		test_panel.add(p6);
 		
 		
 		p2 = new JPanel();
@@ -96,9 +155,16 @@ public class UserMain extends JFrame implements ActionListener  {
 		p2.add(b5);
 		
 		
+		last = new JPanel();
+		last.setBackground(Color.white);
+		last.setLayout(new BorderLayout());
+		last.add(test_panel,BorderLayout.NORTH);
+		last.add(p2);
+		add(last);
 		
-		add(p1,BorderLayout.NORTH);
-		add(p2);
+	
+		
+		
 		setVisible(true);
 		
 		
@@ -113,11 +179,17 @@ public class UserMain extends JFrame implements ActionListener  {
 	public static void main(String[] args) {
 		try {
 			DB.init();
+			
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		new UserMain("이용자", 1020, 420);
+		
+		
+		
+		
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -134,6 +206,45 @@ public class UserMain extends JFrame implements ActionListener  {
 				else if(obj == b5) {
 					new IsolatedLogin("자가격리자 로그인", 710, 520);
 				}
+	}
+	
+	
+	public void covidScoreboard() {
+//		// 실행 간격 지정(3600초)
+//		int sec = 3600;
+//		// 주기적인 작업을 위한
+//		final ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
+//		exec.scheduleAtFixedRate(new Runnable() {
+//			@Override
+//			public void run() {
+				try {
+					
+					doc = Jsoup.connect(url).get();
+					confirmed_elm = doc.select("#_cs_production_type > div > div.main_tab_area > div > div > ul > li.info_01 > em");
+					death_elm = doc.select("#_cs_production_type > div > div.main_tab_area > div > div > ul > li.info_04 > em");
+					totalCon_elm = doc.select("#_cs_production_type > div > div.main_tab_area > div > div > ul > li.info_01 > p");
+					totalDea_elm = doc.select("#_cs_production_type > div > div.main_tab_area > div > div > ul > li.info_04 > p");
+					
+					 today_confirmed = confirmed_elm.text();
+					 today_death = death_elm.text();
+					 today_totalCon = totalCon_elm.text();
+					today_totalDea = totalDea_elm.text();
+					
+					covid_label.setText("일일 확진자 :  " + today_confirmed + "  누적 확진자 :  " + today_totalCon + "  일일 사망자 :  " + today_death + "  ");
+//					local_label.setText("<html><body><center>" +"sdfdsf"+ "</body></html>");
+//					System.out.println("일일 확진자 : " + today_confirmed);
+//					System.out.println("일일 사망자 : " + today_death);
+//					System.out.println("누적 확진자 : " + today_totalCon);
+//					System.out.println("누적 사망자 : " + today_totalDea);
+				} catch (Exception e) {
+					e.printStackTrace();
+
+					// 에러 발생시 중지시킴
+//					exec.shutdown();
+//				}
+//			}
+//		}, 0, sec, TimeUnit.SECONDS);
+	}
 	}
 
 }
