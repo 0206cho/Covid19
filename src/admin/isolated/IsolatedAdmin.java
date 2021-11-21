0e9a3e0166c057a6a8c7ed1ba2e1705c9a7b1a91
0206cho <0206cho@naver.com>
2021-11-21 17:04:06 +0900

    관리자 자가격리자 테이블 수정

--- src/admin/isolated/IsolatedAdmin.java
+++ src/admin/isolated/IsolatedAdmin.java
@@ -24,7 +24,7 @@ import javax.swing.table.DefaultTableModel;
 import jdbc.DB;
 
 public class IsolatedAdmin extends JFrame implements ActionListener {
-	
+
 	private JPanel p1;
 	private JLabel lblicon;
 	private JPanel last;
@@ -47,29 +47,29 @@ public class IsolatedAdmin extends JFrame implements ActionListener {
 	private JButton btnDelete;
 	private JButton btnModify;
 
-	//JFrame을 상속 받아 만드는 방법 << 이걸 더 선호함.
+	// JFrame을 상속 받아 만드는 방법 << 이걸 더 선호함.
 	public IsolatedAdmin(String title, int width, int height) {
 		this.setTitle(title);
 		setSize(width, height);
-		setLocationRelativeTo(this); 	//화면 가운데 찍음
-		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //닫을수 있는 특정 상수값을 주었기 때문에 프레임 종료버튼이 클릭될때 프로그램도 같이 사라짐 
-		
-		
-		//상단 패널
+		setLocationRelativeTo(this); // 화면 가운데 찍음
+		// setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //닫을수 있는 특정 상수값을 주었기 때문에 프레임
+		// 종료버튼이 클릭될때 프로그램도 같이 사라짐
+
+		// 상단 패널
 		p1 = new JPanel();
 		p1.setLayout(new BorderLayout());
 		ImageIcon icon = new ImageIcon("images/logo2.JPG");
 		lblicon = new JLabel(icon);
 		lblicon.setToolTipText("밤낮없이 방역을 위해 고생하시는 관계자분들 응원합니다!!");
-		//lblicon.setPreferredSize(new Dimension(150, 150));
+		// lblicon.setPreferredSize(new Dimension(150, 150));
 		p1.setBackground(Color.white);
-		p1.add(lblicon,BorderLayout.WEST);
-		
-		//상단 버튼 패널
+		p1.add(lblicon, BorderLayout.WEST);
+
+		// 상단 버튼 패널
 		p2 = new JPanel();
 		p2.setBackground(Color.WHITE);
 		p2.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0));
-		
+
 		btnAdd = new JButton("삽입");
 		btnAdd.setBorderPainted(false);
 		btnAdd.setFont(new Font("맑은 고딕", Font.BOLD, 13));
@@ -77,7 +77,7 @@ public class IsolatedAdmin extends JFrame implements ActionListener {
 		btnAdd.setFocusPainted(false);
 		btnAdd.addActionListener(this);
 		p2.add(btnAdd);
-		
+
 		btnDelete = new JButton("삭제");
 		btnDelete.setBorderPainted(false);
 		btnDelete.setFont(new Font("맑은 고딕", Font.BOLD, 13));
@@ -85,7 +85,7 @@ public class IsolatedAdmin extends JFrame implements ActionListener {
 		btnDelete.setFocusPainted(false);
 		btnDelete.addActionListener(this);
 		p2.add(btnDelete);
-		
+
 		btnModify = new JButton("수정");
 		btnModify.setBorderPainted(false);
 		btnModify.setFont(new Font("맑은 고딕", Font.BOLD, 13));
@@ -93,84 +93,78 @@ public class IsolatedAdmin extends JFrame implements ActionListener {
 		btnModify.setFocusPainted(false);
 		btnModify.addActionListener(this);
 		p2.add(btnModify);
-		
-		
+
 		pNor = new JPanel();
 		pNor.setLayout(new BorderLayout());
 		pNor.add(p1, BorderLayout.WEST);
 		pNor.add(p2, BorderLayout.EAST);
 		pNor.setBackground(Color.WHITE);
-		
-		
-		//중앙패널
+
+		// 중앙패널
 		pCen = new JPanel();
 //		p2.setLayout(new BorderLayout());
 		pCen.setBackground(Color.white);
-		
-		
-		String [] column = { "ID", "이름", "PW", "지역", "휴대폰", "날짜"};
-		
-		model = new DefaultTableModel(column, 0);
+
+		String[] column = { "ID", "이름", "PW", "지역", "휴대폰", "날짜" };
+
+		model = new DefaultTableModel(column, 0) { // 테이블 더블 클릭시 내용 수정 불가
+			public boolean isCellEditable(int i, int c) {
+				return false;
+			}
+		};
+
 		table = new JTable(model);
+		table.getTableHeader().setReorderingAllowed(false); // 테이블 열 이동 불가
 		sp = new JScrollPane(table);
 		sp.setPreferredSize(new Dimension(580, 500));
-		
+
 		SelectAll(model);
-		
+
 		pCen.add(sp);
-		
-		
-		
 
-
-		
-		
-		//모든 패널 붙이기
+		// 모든 패널 붙이기
 		last = new JPanel();
 		last.setLayout(new BorderLayout());
 		last.setBackground(Color.white);
-		last.add(pNor,BorderLayout.NORTH);
+		last.add(pNor, BorderLayout.NORTH);
 		last.add(pCen);
 		add(last);
-		
+
 		setVisible(true);
-		
-		
+
 	}
+
 	public void SelectAll(DefaultTableModel model) {
 		String sql = "select * from isolated";
-		
+
 		try {
 			ResultSet rs = DB.getResultSet(sql);
-			//전체데이터 삭제후 다시 출력
-			for(int i = 0; i < model.getRowCount();) {
+			// 전체데이터 삭제후 다시 출력
+			for (int i = 0; i < model.getRowCount();) {
 				model.removeRow(0);
 			}
-			
-			while(rs.next()) {
-				String data[] = { rs.getString(1), rs.getString(2), rs.getString(3), 
-						rs.getString(4), rs.getString(5), rs.getString(6)
-				};
+
+			while (rs.next()) {
+				String data[] = { rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+						rs.getString(6) };
 				model.addRow(data);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	
+
 	public DefaultTableModel getModel() {
 		return model;
 	}
-	
-	public void paint (Graphics g) {
+
+	public void paint(Graphics g) {
 		super.paint(g);
 		g.drawLine(0, 100, 1050, 100);
-		
 
 	}
+
 	public static void main(String[] args) {
 		try {
 			DB.init();
@@ -178,23 +172,23 @@ public class IsolatedAdmin extends JFrame implements ActionListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		new IsolatedAdmin("자가격리괸리", 600, 620);
 	}
-	
-	//이벤트 처리
+
+	// 이벤트 처리
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		
-		if(obj == btnAdd) {
+
+		if (obj == btnAdd) {
 			new IsolatedAdd("격리자삽입", 300, 320, this);
-		}else if(obj == btnDelete) {
+		} else if (obj == btnDelete) {
 			new IsolatedDelete("격리자삭제", 300, 300, this);
-		}else if(obj == btnModify) {
+		} else if (obj == btnModify) {
 			new IsolatedModify("격리자수정", 300, 320, this);
 		}
-		
+
 	}
 
 }
