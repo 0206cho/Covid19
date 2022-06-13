4b8123633b08a41653637a0b5aafcb68fa754168
MinSol <68586044+0206cho@users.noreply.github.com>
2022-06-13 16:19:21 +0900

    Update DB.java

--- src/jdbc/DB.java
+++ src/jdbc/DB.java
@@ -15,7 +15,7 @@ public class DB {
 	public static void init() throws Exception {
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		conn = DriverManager.getConnection("jdbc:mysql://114.71.137.174:61083/covid", "covid","0206");
+		conn = DriverManager.getConnection("jdbc:mysql://IP:PORT/covid", "covid","0206");
 		stmt = conn.createStatement();
 		System.out.println("DB 연결성공");
 		 
